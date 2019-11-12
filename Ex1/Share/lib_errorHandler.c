d4a6b1b2e4697336f37f95fdfadf528e9c7791c4
tomerperetz <tomerperetz@gmail.com>
2019-11-12 19:48:16 +0200

    bug fixed: h file declaration is now the same is the function header. exit function added to error raise

--- Ex1/Share/lib_errorHandler.c
+++ Ex1/Share/lib_errorHandler.c
@@ -1,10 +1,11 @@
 #include "lib_errorHandler.h"
 
-void raiseError(int error_id, char *file_path, int line, char *function_name, char *description) {
+void raiseError(int error_id, char *function_name , char *file_path, int line, char *description)
+{
 	printf("FATAL ERROR: ERROR NUMBER %d\n", error_id);
 	printf("File: %s\n", file_path);
 	printf("Function: %s\n", function_name);
 	printf("Line: %d\n", line);
 	printf("Description: %s\n", description);
+	exit(ERROR);
 }
-
--- Ex1/Share/lib_errorHandler.h
+++ Ex1/Share/lib_errorHandler.h
@@ -4,13 +4,11 @@
 #endif
 
 #include "hardCodedData.h"
+//
+//static const int ERROR_ID_1 = 1;
+//static const int ERROR_ID_GLOBAL= -1;
+//static const char ERROR_ID_1_CONTENT[160] = \
+//"The given arguments are not in the correct format / number of arguments is incorrect.\n"\
+//"For more help please run the following command: ~<Project-Name> --help\n";
 
-static const int ERROR_ID_1 = 1;
-static const int ERROR_ID_GLOBAL= -1;
-static const char ERROR_ID_1_CONTENT[160] = \
-"The given arguments are not in the correct format / number of arguments is incorrect.\n"\
-"For more help please run the following command: ~<Project-Name> --help\n";
-
-void raiseError(int error_id, char *file_path,char *line, char *function_name);
-
-
+void raiseError(int error_id, char *function_name, char *file_path, int line, char *description);
