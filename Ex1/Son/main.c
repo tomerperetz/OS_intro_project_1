f4c73ffd508e8697b57cd5fbec1f91686d19116c
tomerperetz <tomerperetz@gmail.com>
2019-11-12 19:47:21 +0200

    main integration with segev scripts: defines inserted, prints deleted. still need to delete final result delete

--- Ex1/Son/main.c
+++ Ex1/Son/main.c
@@ -1,73 +1,59 @@
-#include <stdio.h>
+
 #include "../Share/lib_str_func.h"
+#include "../Share/argparser.h"
 
 
-
-#define YES 0 //need to delete
 
 int calcEquation(int x, int y, char eq_operator)
 {
+	//char operator_error[60] = "Can't use supplied opertor: %c\nExiting Program\n";
+
 	if (eq_operator == '+')
 		return (x + y);
 	else if (eq_operator == '*')
 		return (x*y);
 	else
 	{
-		printf("cant use supplied opertor: %c\nExiting Program\n", eq_operator);
-		exit(1);
+		raiseError(1, __FILE__, __func__, __LINE__, ARGS_ERROR);
+		return ERROR;
 	}
 }
 
 int main(int argc, char *argv[])
-{
-	// insert defines
-
-	char user_equation[32];
-	int x = -1, y=-1 , operator_index = -1, result=-1;
+{	
 	
-	printf("Program name %s\n", argv[0]);
 
-	// verify input is valid
-	if (argc == 2) {
-		printf("The argument supplied is %s\n", argv[1]);
-	}
-	else if (argc > 2) {
-		printf("Too many arguments supplied.\nPlease restart program with a valid input.");
-		exit(1);
-	}
-	else {
-		printf("One argument  .\nPlease restart program with a valid input.");
-		exit(1);
-	}
+	char user_equation[MAX_SON_ARG_INPUT_LEN];
+	int x = ERROR, y= ERROR, operator_index = ERROR, result = ERROR;
 
-	if (YES == strcpy_s(user_equation, 32, argv[1]))
+	// args parser
+	if (ensureArgs(argc, 1, argv) == FALSE)
+			raiseError(2, __FILE__, __func__, __LINE__, ARGS_ERROR);
+
+	if (YES == strcpy_s(user_equation, MAX_SON_ARG_INPUT_LEN, argv[1]))
 	{
 		// Search for '+' arithmetic operator index
 		operator_index = getCharindex('+', user_equation);
 
 		// if '+' wasn't found look for '*'
-		if (operator_index == -1)
+		if (operator_index == ERROR)
 			operator_index = getCharindex('*', user_equation);
 
 		// verify operator was found
-		if (operator_index == -1)
-		{
-			printf("couldn't locate arithmetical operator.\nPlease use '+' or '-' only.\nExiting program.\n");
-			exit(1);
-		}
-
-		
-		x = getFirstIntFromString(user_equation, operator_index);
-		y = getLastIntFromString(user_equation, operator_index + 1);
-		result = calcEquation(x, y, user_equation[operator_index]);
-
-		printf("result: %d", result);
-
-		return 0;
+		if (operator_index == ERROR)
+			raiseError(1, __FILE__, __func__, __LINE__, ARGS_ERROR);
 	}
-		
 
-	return 0;
+		
+	// calculate
+	x = getFirstIntFromString(user_equation, operator_index);
+	y = getLastIntFromString(user_equation, operator_index + 1);
+	result = calcEquation(x, y, user_equation[operator_index]);
+
+	//need to delete
+	printf("result: %d", result);
+
+	return result;	
 }
  
 
